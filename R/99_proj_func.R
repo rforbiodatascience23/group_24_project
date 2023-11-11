2279650e5770faacd614f309402f2562add4b3dc
LyroudisK <s232994@dtu.dk>
2023-11-11 19:38:16 +0100

    99_proj_func.R file creation and test

---
+++ R/99_proj_func.R
@@ -0,0 +1,8 @@
+# test
+# treatment_cols <- function(column){
+#   if (str_detect(column, ".DMSO")){
+#     str_replace_all(column, ".DMSO", "_untreated")
+#   } else if (str_detect(column, ".G007.LK")){
+#     str_replace_all(column, ".GOO7.LK", "_treated")
+#   }
+# }
