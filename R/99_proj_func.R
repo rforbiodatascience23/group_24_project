288b0a7b3d5e4fd3c0ce7cbb2d264658711ca253
LyroudisK <s232994@dtu.dk>
2023-11-13 18:58:27 +0100

    Work (nearly final) on 99_proj_func and load.qmd

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -1,10 +1,12 @@
 download_csv <- function(file_url, file_name) {
+  raw_dir <- "../data/_raw"
+  data_dir <- "../data/"
   # Download file if it doesn't exist
   if (!file.exists(str_c(raw_dir, file_name, ".csv"))) {
     download.file(url = file_url, destfile = str_c(raw_dir, file_name, ".csv"))
   }
   # Read data into a data frame
-  df <- read_csv(file = str_c(raw_dir, file_name, ".csv"), delim = ",")
+  df <- read_csv(file = str_c(raw_dir, file_name, ".csv"))
   
   # Write data to a TSV file
   write_tsv(x = df, file = str_c(data_dir, file_name, ".tsv.gz"))
@@ -15,9 +17,11 @@ download_csv <- function(file_url, file_name) {
 
 
 
--------------------------------------------------------------------------------
+###############################################################################
   
 download_txt <- function(file_url, file_name) {
+  raw_dir <- "../data/_raw"
+  data_dir <- "../data/"
   # Download file if it doesn't exist
   if (!file.exists(str_c(raw_dir, file_name, ".txt"))) {
     download.file(url = file_url, destfile = str_c(raw_dir, file_name, ".txt"))
@@ -34,4 +38,25 @@ download_txt <- function(file_url, file_name) {
 
 
 
--------------------------------------------------------------------------------
+###############################################################################
+
+generate_dir <- function(){
+  data_dir <- "../data/"
+  if( !dir.exists(data_dir) ){
+    dir.create(path = data_dir)
+  }
+  
+  raw_dir <- "../data/_raw/"
+  if( !dir.exists(raw_dir) ){
+    dir.create(path = raw_dir)
+  }
+}
+
+
+
+###############################################################################
+
+head_all <- function(alist){
+  map(.x = alist,
+      .f = ~head(.x)) 
+}
