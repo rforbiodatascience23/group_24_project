34c7637b5b9df00ee97ca2ff12fad248637266c3
LyroudisK <s232994@dtu.dk>
2023-11-13 11:30:51 +0100

    Deleted code for "download data from Github" approach

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -6,3 +6,20 @@
 #     str_replace_all(column, ".GOO7.LK", "_treated")
 #   }
 # }
+
+
+#maybe a function for transform our uploading datasets
+#but needs improvement
+#file_tr <- function(origin,filename){
+# x<-vroom::vroom("../_raw/origin",
+#              show_col_types = FALSE)
+#  write.table(x,
+#          file = "../data/filename.tsv",
+#         sep = "\t",
+#        row.names = FALSE, 
+#       quote = FALSE)
+
+#filename<- vroom::vroom("../data/filename.tsv", 
+#                       show_col_types = FALSE)
+
+#}
